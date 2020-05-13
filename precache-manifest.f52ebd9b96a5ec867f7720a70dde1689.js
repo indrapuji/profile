015683280bdf27ae5a63e0125b021fa9ff64ced4
indrapuji <47031836+indrapuji@users.noreply.github.com>
2020-05-14 01:12:28 +0700

    Updates

---
+++ precache-manifest.f52ebd9b96a5ec867f7720a70dde1689.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b375cbce0ef680f4d1b2feeff5aae8",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "bc85e7eae79ff601b83f",
+    "url": "/profile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af7ddf3b28ba24f85864",
+    "url": "/profile/static/js/2.e7d76702.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profile/static/js/2.e7d76702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc85e7eae79ff601b83f",
+    "url": "/profile/static/js/main.0b2220a6.chunk.js"
+  },
+  {
+    "revision": "f94e4e236317dcd7c074",
+    "url": "/profile/static/js/runtime-main.629e9787.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
