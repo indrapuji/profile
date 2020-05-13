1d96301d0bb6e0ff98f98fbd91127cbe6010b4be
indrapuji <47031836+indrapuji@users.noreply.github.com>
2020-05-14 02:54:25 +0700

    another test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Home, Skills, Experience } from "./pages";
 
 function App() {
 	return (
 		<Router>
+			<Link to="/profile">Home</Link>
+			<Link to="/profile/skills">Skills</Link>
+			<Link to="/profile/experience">Experience</Link>
 			<Switch>
 				<Route exact path="/profile">
 					<Home />
